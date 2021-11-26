13c2dfb9b5ac102ff218be8d61990a47aa2c7bd5
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-27 01:16:00 +0330

    initail commit

---
+++ instagram/urls.py
@@ -0,0 +1,33 @@
+"""instagram URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework_jwt.views import obtain_jwt_token
+
+from user.views import ProfileDetailView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_jwt_token),
+    path('auth/', include('user.urls')),
+    path('content/', include('content.urls')),
+    path('activity/', include('activity.urls')),
+    path('api/auth/', include('user.api.urls')),
+    path('relation/', include('relation.urls')),
+    path('', TemplateView.as_view(template_name='user/home.html')),
+    path('<str:username>/', ProfileDetailView.as_view(), name='profile')
+]
